e11c3daaf8fca9a3a53aa677d62b7e56cfb26539
Paul Schweizer <paulschweizer@gmx.net>
2020-02-29 18:14:21 +0100

    Create QApplication

--- test_flowpipe_editor.py
+++ test_flowpipe_editor.py
@@ -5,9 +5,13 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), "NodeGraphQt-master"))
 sys.path.append(os.path.join(os.path.dirname(__file__)))
 
-
+from Qt import QtWidgets
 from floweditor import editor
 
 
+app = QtWidgets.QApplication(sys.argv)
+
 w = editor.FlowEditor()
 w.show()
+
+sys.exit(app.exec_())
